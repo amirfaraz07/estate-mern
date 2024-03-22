0effe71a2bf1162663a21174b682f2ef86db303f
amirfaraz07 <amirfaraz07@gmail.com>
2024-03-22 19:12:48 +0500

    error

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const SignUp = () => {
+export default function SignUp() {
+
+  const [ formData, setFormData ] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch('/api/auth/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    console.log(data);
+  }
+
+  const handleChange = (e) => {
+    setFormData(
+      {
+        ...formData,
+        [e.target.id] : e.target.value,
+      }
+    )
+  }
+  console.log(formData);
   return (
-    <div>SignUp</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+
+        <input type='text' placeholder='Username' className='border p-3 rounded-lg' id='username' onChange={handleChange} />
+
+        <input type='email' placeholder='Email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
+
+        <input type='password' placeholder='Password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
+
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
+        </form>
+
+        <div className='flex gap-2 mt-5'>
+          <p>Have an account?</p>
+          <Link to={"/sign-in"}>
+            <span className='text-blue'>Sign in</span>
+          </Link>
+        </div>
+
+      </div>
   )
 }
-
-export default SignUp
